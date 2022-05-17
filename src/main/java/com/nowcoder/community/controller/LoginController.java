6a63f9c0f8c51ab9adab791a1f686553a9a85112
youngbill-999 <63612702+youngbill-999@users.noreply.github.com>
2022-05-17 17:05:19 +0800

    Changes

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.nowcoder.community.controller;
 
 
+import com.google.code.kaptcha.Producer;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.http.HttpResponse;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
+    private  static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegister(){
         return "/site/register";
@@ -57,6 +68,24 @@ public class LoginController implements CommunityConstant {
         }
         return "/site/operate-result";
     }
+    @Autowired
+    private Producer producer;
+    @RequestMapping(path="/kaptcha",method = RequestMethod.GET)
 
+    public void getKaptcha(HttpServletResponse response, HttpSession session)
+   {
+            String text = producer.createText();
+            BufferedImage image = producer.createImage(text);
+       session.setAttribute("kaptcha",text);
+       response.setContentType("image/png");
+       try {
+           OutputStream os = response.getOutputStream();
+           ImageIO.write(image,"png",os);
+       } catch (IOException e) {
+           logger.error("Failed to request a verification code!"+e.getMessage());
+       }
+
+
+   }
 
 }
